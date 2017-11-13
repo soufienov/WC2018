fcd396571966df814e69b141afae3815481eda80
soufienov <soufienov@gmail.com>
2017-11-13 11:19:04 +0100

    Add project files.

---
+++ WC2018/WC2018/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WC2018
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
